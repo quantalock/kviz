7a87bbaa4ae08c39a525fab3e1957acc632ebec6
quantalock <jaroslav.hercik@quantalock.cz>
2025-04-30 09:35:07 +0200

    Add files via upload

---
+++ otazky.js
@@ -0,0 +1,26 @@
+const otazky = [
+  {
+    id: 1,
+    obtiznost: 3,
+    nazev: "Mlynářův kámen",
+    otazka: "Nad Moravanským potokem nedaleko Dubic je krásný výhled na údolí Labe z vyhlídky, která připomíná zašlé řemeslo.",
+    moznosti: ["Kovářský kámen", "Mlynářský kámen", "Uhelný kámen"],
+    spravna: "Mlynářův kámen"
+  },
+  {
+    id: 2,
+    obtiznost: 1,
+    nazev: "Milešovka",
+    otazka: "Královna středohoří dosahuje výšky 837 m n.m. Dlouho byla považována za nejvyšší horu Čech.",
+    moznosti: ["Milešovka", "Albertovka", "Kletečná"],
+    spravna: "Milešovka"
+  },
+  {
+    id: 3,
+    obtiznost: 2,
+    nazev: "Kletečná",
+    otazka: "Sestra Milešovky, Malá Milešovka, to jsou jiná jména této 706 m n.m. vysoké hory se zalesněnou špičkou.",
+    moznosti: ["Panna", "Vysoká", "Kletečná"],
+    spravna: "Kletečná"
+  },
+];
